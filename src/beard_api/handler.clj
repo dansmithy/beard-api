3d822f674cf0f2663ba3d62651a62e0575574132
Danny Smith <danny.j.smith@oracle.com>
2017-11-23 10:06:58 +0000

    Adding an /address endpoint.

--- src/beard_api/handler.clj
+++ src/beard_api/handler.clj
@@ -5,7 +5,8 @@
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
             [ring.middleware.json :as json]
-            [config.core :as config.core]))
+            [config.core :as config.core])
+  (:import [java.net InetAddress UnknownHostException]))
 
 (defonce env
   (merge
@@ -22,6 +23,10 @@
    {:name "Full beard"}
    {:name "Garibaldi"}])
 
+(defn get-ip-address
+  []
+  (str (InetAddress/getLocalHost)))
+
 (defroutes app-routes
   (GET "/" []
     {:status 200
@@ -37,6 +42,11 @@
                                    :query-string
                                    :scheme
                                    :request-method])})
+
+  (GET "/address" []
+    {:status 200
+     :body {:address (get-ip-address)}})
+
   (route/not-found "Not Found"))
 
 (def app
