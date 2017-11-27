2d353e3053fc41a788501cfb68148f1b1d6e370a
Danny Smith <danny.j.smith@oracle.com>
2017-11-27 09:53:25 +0000

    Using the full DNS name for moustache service.

--- src/beard_api/moustache.clj
+++ src/beard_api/moustache.clj
@@ -8,7 +8,7 @@
   []
   (try
     (let [url (format "http://%s"
-                      (config.core/env :moustache-api-base-url "moustache-api:30001"))
+                      (config.core/env :moustache-api-base-url "moustache-api.default.svc.cluster.local:30001"))
           response
           (http/request
            {:method :get
